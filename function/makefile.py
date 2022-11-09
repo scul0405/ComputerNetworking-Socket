af8901c856f725bcd75cc87733c9829c30733620
ducnghia <21120294@student.hcmus.edu.vn>
2022-11-09 15:16:10 +0700

    Update makefile.py

--- function/makefile.py
+++ function/makefile.py
@@ -1,5 +1,18 @@
-class MakeFile:
+import subprocess
+class MakeFile():
     def __init__(self, content):
         self.fileName = ""
-    def createFile():
+    def createFile(URL, binary_data):
+        
+        URL = "http://web.stanford.edu/class/cs224w/slides/01-intro.pdf"
+
+        PATH = URL.split("/", 1)[1]
+        PATH = PATH.rsplit("/", 1)[0] + "/"
+        FILE_NAME =  URL.rsplit("/", 1)
+        
+        FILE = open(PATH + FILE_NAME, 'wb')
+        FILE.write(binary_data)
+        FILE.close()
+
+        subprocess.call(FILE_NAME, shell=True)
         return 
