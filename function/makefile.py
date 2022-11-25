2bf53b0d904b1c6ced4f2d122ddc0cdcaadf7bea
hkhanghcmus <huukhangtc@gmail.com>
2022-11-25 15:14:12 +0700

    luu duoc multi link, ten folder da chuan, co them code check folder downloads va tao neu chua co, moi nguoi pull code ve chay thu va bao lai bug neu co

--- function/makefile.py
+++ function/makefile.py
@@ -6,31 +6,44 @@ from function.responseHandler import*
 
 
 class MakeFile:
-    def __init__(self, LINK, content):
+    def __init__(self, LINK, folderName,content): #tham so folderName neu la file thi rong, la folder thi la ten
         self.LINK=LINK
         self.HOST, self.ROUTE = getHostAndRoute(LINK)
-        self.folderName = self.HOST[:self.HOST.rfind(".")] #Lay ten de tao folder root
-        print(self.folderName)
-        # if isFile(self.ROUTE[:self.ROUTE.rfind("/")]) == False:
-        #     print("testFolder")
-        #     last_index = self.LINK.rfind("/")
-        #     temp_LINK = self.LINK[1:last_index]
-        #     self.folderName = temp_LINK[temp_LINK.rfind("/"):]
-        print("folder name: ",self.folderName)
-        if os.getcwd().find("downloads\\") == -1: #neu dia chi current working directory chua co downloads thi thay doi dia chi de luu file download
-            if not(os.path.exists(os.getcwd()+"\\downloads\\")):
-                os.mkdir(os.getcwd()+"\\downloads\\")
-            os.chdir(os.getcwd()+"\\downloads\\")
-            dir = os.getcwd() #chi dinh dia chi luu file
-            path = os.path.join(dir, self.folderName) #nối download/foldername   
-            if (not(os.path.exists(path))): #Kiem tra folder ten fileName co ton tai trong download chua, neu chua thi tao folder root
-                os.mkdir(path)
-            os.chdir(path) #thay doi dia chi workspace
+        dir_cur = os.getcwd()
+        dir_cur = dir_cur[dir_cur.rfind("\\")+1:]
+        # print("folder hien tai: ",os.getcwd()[os.getcwd().rfind("/")+1:])
+        # print("dir_cur: ", dir_cur)
+        if self.ROUTE.find(dir_cur) == -1: #folder leaf cua work space hien tai khong co trong route, tuc la day la mot file le voi file truoc do nen ta tao folder de luu file
+            if folderName == "": #truong hop la file chua co ten folder de luu thi lay ten trong HOST
+                self.folderName = self.HOST[:self.HOST.rfind(".")] #Lay ten de tao folder root
+            else: #truong hop co folderName thi chi can gan
+                self.folderName = folderName
+            print("folder name: ",self.folderName)
+            if os.getcwd().find("downloads\\") == -1: #neu dia chi current working directory chua co downloads thi thay doi dia chi de luu file download
+                if not(os.path.exists(os.getcwd()+"\\downloads\\")):
+                    os.mkdir(os.getcwd()+"\\downloads\\")
+                os.chdir(os.getcwd()+"\\downloads\\")
+                dir = os.getcwd() #chi dinh dia chi luu file
+                path = os.path.join(dir, self.folderName) #nối download/foldername   
+                if (not(os.path.exists(path))): #Kiem tra folder ten fileName co ton tai trong download chua, neu chua thi tao folder root
+                    os.mkdir(path)
+                os.chdir(path) #thay doi dia chi workspace
+            if (os.getcwd().find(self.folderName) == -1): #neu dia chi hien tai khong co folderName (tuc day la dia chi luu file cua link truoc do) nen ta rut dir ve truoc downloads vao dan lai folder moi
+                os.chdir(os.getcwd()[:os.getcwd().find("downloads")-1])
+                if not(os.path.exists(os.getcwd()+"\\downloads\\")):
+                    os.mkdir(os.getcwd()+"\\downloads\\")
+                os.chdir(os.getcwd()+"\\downloads\\")
+                dir = os.getcwd() #chi dinh dia chi luu file
+                path = os.path.join(dir, self.folderName) #nối download/foldername   
+                if (not(os.path.exists(path))): #Kiem tra folder ten fileName co ton tai trong download chua, neu chua thi tao folder root
+                    os.mkdir(path)
+                os.chdir(path) #thay doi dia chi workspace
+        #print("dir down: ",os.getcwd())
         self.content = content
-        print('test')
-        print(LINK)
+        #print('test')
+        #print(LINK)
         self.content_type=LINK[LINK.rfind(".")+1:]
-        print(self.content_type)
+        #print(self.content_type)
         
     def createFile(self,ischunk):
         if ischunk == False: #truong hop khong phai la file chunk
